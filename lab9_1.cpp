3b6187328e6328f95db49bbac7e2f0479b5bb7c7
Ratchathon <Ratchathon_S@cmu.ac.th>
2024-12-17 19:54:36 +0700

    lab9_1 finish

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,22 +1,19 @@
 #include<iostream>
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
-#include<cmath>
 
 int main(){	
-	int loan , pay ,i = 1;
-	float inter,total,New ;
-	double dok;
+	double moneyl,rate,moneyp,total,PB,interest,nb;
 	cout << "Enter initial loan: ";
-	cin>>loan;
+	cin >> moneyl;
 	cout << "Enter interest rate per year (%): ";
-	cin>>inter;
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
-	cin>>pay;
-	dok = loan*(inter/100);
-	total = loan+dok;
-	New = total - pay;
+	cin >> moneyp;
 	
+	//use 'setw' to set width of table and 'left' to set left-alignment
+	//you can change input argument of 'setw()' to see the effect
+	//Try to change from 'left' to 'right' and see the effect
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -25,17 +22,41 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	cout << fixed << setprecision(2); 
-	//for(i==1;New<=0;i++){
-	cout << setw(13) << left << i; 
-	cout << setw(13) << left << loan;
-	cout << setw(13) << left << dok;
-	cout << setw(13) << left << total;
-	cout << setw(13) << left << pay;
-	cout << setw(13) << left << New;
-	cout << "\n";	
-	//}
-	
-	
+	int i=0;
+	while (true) {
+		PB=moneyl;
+		interest=moneyl*(rate/100);
+		total=moneyl+interest;
+		nb=total-moneyp;
+
+		if (nb>0){
+			moneyl=nb;
+			cout << fixed << setprecision(2); 
+			cout << setw(13) << left << i+1; 
+			cout << setw(13) << left << PB;
+			cout << setw(13) << left << interest;
+			cout << setw(13) << left << total;
+			cout << setw(13) << left << moneyp;
+			cout << setw(13) << left << nb;
+			cout << "\n";
+			++i;
+		}
+		else if(nb<0){
+			if (total<moneyp){moneyp=total;}
+			nb=0;
+			cout << fixed << setprecision(2); 
+			cout << setw(13) << left << i+1; 
+			cout << setw(13) << left << PB;
+			cout << setw(13) << left << interest;
+			cout << setw(13) << left << total;
+			cout << setw(13) << left << moneyp;
+			cout << setw(13) << left << nb;
+			cout << "\n";
+			break;
+		}
+	}
+	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
+	//you can change input argument of 'setprecision()' to see the effect
+
 	return 0;
 }
